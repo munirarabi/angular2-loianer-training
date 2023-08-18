8e7f2cb477e41693ec13ef1b8db9ee9cf4fb8b35
Munir Marques <munir.marques@msc.com>
2023-08-18 15:09:04 -0300

    adicionando as outras pastas

---
+++ hello-typescript/main.js
@@ -0,0 +1,11 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (number) {
+    console.log(number);
+});
+var n1 = 'dada';
---
+++ hello-typescript/main.ts
@@ -0,0 +1,15 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x+y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map((number) => {
+    console.log(number)
+})
+
+var n1: string = 'dada';
